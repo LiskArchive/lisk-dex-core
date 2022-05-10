72388886c27f94a0ee574718b8cbbbf6fae3a003
10dimensions <relay.10d@gmail.com>
2022-05-10 14:28:32 +0530

    Initialize dex module

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
+import { DexModule } from "./modules/dex/dex_module";
 
 // @ts-expect-error Unused variable error happens here until at least one module is registered
-export const registerModules = (_app: Application): void => {};
+export const registerModules = (_app: Application): void => {
+
+    app.registerModule(DexModule);
+};
