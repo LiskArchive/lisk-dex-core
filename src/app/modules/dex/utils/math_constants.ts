1b2f6f1548b7c6921d9675fd2252a7e34cfbdbad
10dimensions <38999489+10dimensions@users.noreply.github.com>
2022-06-13 13:38:43 +0530

    Fix BigInt SqRt Computation

--- src/app/modules/dex/utils/math_constants.ts
+++ src/app/modules/dex/utils/math_constants.ts
@@ -13,9 +13,35 @@
  */
 
 import JSBI from 'jsbi';
+import invariant from 'tiny-invariant'
 
-// Q96 Number System
+export const MAX_SAFE_INTEGER = JSBI.BigInt(Number.MAX_SAFE_INTEGER)
+
+export const ZERO = JSBI.BigInt(0)
 export const N_96 = JSBI.BigInt(96);
 export const ONE = JSBI.BigInt(1);
 export const TWO = JSBI.BigInt(2);
-export const SQRT_EXP = JSBI.divide(ONE, TWO);
+//export const SQRT_EXP = JSBI.divide(ONE, TWO);
+
+/**
+ * Computes floor(sqrt(value))
+ * @param value the value for which to compute the square root, rounded down
+ */
+export const sqrt = (value: JSBI): JSBI => {
+  invariant(JSBI.greaterThanOrEqual(value, ZERO), 'NEGATIVE')
+
+  // rely on built in sqrt if possible
+  if (JSBI.lessThan(value, MAX_SAFE_INTEGER)) {
+    return JSBI.BigInt(Math.floor(Math.sqrt(JSBI.toNumber(value))))
+  }
+
+  let z: JSBI
+  let x: JSBI
+  z = value
+  x = JSBI.add(JSBI.divide(value, TWO), ONE)
+  while (JSBI.lessThan(x, z)) {
+    z = x
+    x = JSBI.divide(JSBI.add(JSBI.divide(value, x), x), TWO)
+  }
+  return z
+}
