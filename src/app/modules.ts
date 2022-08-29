f7e29e5ffb2e7d97f9a4f4340803e6b70f1305e1
Emilio <emilio.perez.sanchez@lightcurve.io>
2022-08-29 09:27:48 +0200

    DEX Module, fee tiers, add tests

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
+import { DexModule } from './modules/dex/module';
 
-export const registerModules = (_app: Application): void => {};
+export const registerModules = (app: Application): void => {
+    app.registerModule(new DexModule());
+};
