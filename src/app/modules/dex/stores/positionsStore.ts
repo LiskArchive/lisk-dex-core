28372bc5d8085a624b8b7b0d7dad98c44cd0f6c0
Emilio <emilio.perez.sanchez@lightcurve.io>
2022-09-23 09:49:39 +0200

    Create stores

---
+++ src/app/modules/dex/stores/positionsStore.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import {
+    BaseStore
+} from 'lisk-sdk';
+import {
+    MAX_NUM_BYTES_Q96, NUM_BYTES_ADDRESS
+} from '../constants';
+
+export interface PositionsStoreData {
+    tickLower: number;
+    tickUpper: number;
+    liquidity: bigint;
+    feeGrowthInsideLast0: Buffer;
+    feeGrowthInsideLast1: Buffer;
+    ownerAddress: Buffer;
+}
+
+export const positionsStoreSchema = {
+    $id: '/dex/store/positions',
+    "type": "object",
+    "required": [
+        "tickLower",
+        "tickUpper",
+        "liquidity",
+        "feeGrowthInsideLast0",
+        "feeGrowthInsideLast1",
+        "ownerAddress"
+    ],
+    "properties": {
+        "tickLower": {
+            "dataType": "sint32",
+            "fieldNumber": 1
+        },
+        "tickUpper": {
+            "dataType": "sint32",
+            "fieldNumber": 2
+        },
+        "liquidity": {
+            "dataType": "uint64",
+            "fieldNumber": 3
+        },
+        "feeGrowthInsideLast0": {
+            "dataType": "bytes",
+            "maxLength": MAX_NUM_BYTES_Q96,
+            "fieldNumber": 4
+        },
+        "feeGrowthInsideLast1": {
+            "dataType": "bytes",
+            "maxLength": MAX_NUM_BYTES_Q96,
+            "fieldNumber": 5
+        },
+        "ownerAddress": {
+            "dataType": "bytes",
+            "length": NUM_BYTES_ADDRESS,
+            "fieldNumber": 6
+        }
+    }
+};
+
+export class PositionsStore extends BaseStore < PositionsStoreData > {
+    public schema = positionsStoreSchema;
+}
