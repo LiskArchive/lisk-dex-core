cb3d2913b4c144bf8f2d235391cc008869bb644a
Emilio <emilio.perez.sanchez@lightcurve.io>
2022-09-23 09:49:39 +0200

    Create stores

---
+++ src/app/modules/dex/stores/poolsStore.ts
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import { BaseStore } from 'lisk-sdk';
+import { MAX_NUM_BYTES_Q96 } from '../constants';
+
+export interface PoolsStoreData {
+	liquidity: bigint;
+    sqrtPrice: Buffer;
+    feeGrowthGlobal0: Buffer;
+    feeGrowthGlobal1: Buffer;
+    tickSpacing: number;
+}
+
+export const poolsStoreSchema = {
+	$id: '/dex/store/pools',
+    "type": "object",
+    "required": [
+        "liquidity",
+        "sqrtPrice",
+        "feeGrowthGlobal0",
+        "feeGrowthGlobal1",
+        "tickSpacing"
+    ],
+    "properties": {
+        "liquidity": {
+            "dataType": "uint64",
+            "fieldNumber": 1
+        },
+        "sqrtPrice": {
+            "dataType": "bytes",
+            "maxLength": MAX_NUM_BYTES_Q96,
+            "fieldNumber": 2
+        },
+        "feeGrowthGlobal0": {
+            "dataType": "bytes",
+            "maxLength": MAX_NUM_BYTES_Q96,
+            "fieldNumber": 3
+        },
+        "feeGrowthGlobal1": {
+            "dataType": "bytes",
+            "maxLength": MAX_NUM_BYTES_Q96,
+            "fieldNumber": 4
+        },
+        "tickSpacing": {
+            "dataType": "uint32",
+            "fieldNumber": 5
+        }
+    }
+};
+
+export class PoolsStore extends BaseStore<PoolsStoreData> {
+	public schema = poolsStoreSchema;
+}
