e141aa58b4141abba5f5cbb1bf9e6a5233e97b5b
Emilio <emilio.perez.sanchez@lightcurve.io>
2022-10-13 10:25:51 +0200

    Initialize DEX Rewards module

---
+++ src/app/modules/dexRewards/module.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { BaseCommand, BaseModule, ModuleMetadata, TokenMethod, ValidatorsMethod } from 'lisk-sdk';
+
+import { DexRewardsEndpoint } from './endpoint';
+
+import { DexRewardsMethod } from './method';
+
+export class DexRewardsModule extends BaseModule {
+	public endpoint = new DexRewardsEndpoint(this.stores, this.offchainStores);
+	public method = new DexRewardsMethod(this.stores, this.events);
+	public _tokenMethod = new TokenMethod(this.stores, this.events, this.name);
+	public _validatorsMethod = new ValidatorsMethod(this.stores, this.events);
+
+	public commands = [];
+
+	public metadata(): ModuleMetadata {
+		return {
+			name: this.name,
+			endpoints: [],
+			commands: this.commands.map((command: BaseCommand) => ({
+				name: command.name,
+				params: command.schema,
+			})),
+			events: this.events.values().map(v => ({
+				name: v.name,
+				data: v.schema,
+			})),
+			assets: [],
+		};
+	}
+}
