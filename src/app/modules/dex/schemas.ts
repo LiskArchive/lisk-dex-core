15c7311c2c7551caaff883502b4ac4ebe1c9a77a
Irfan <44484700+Irfan-Personal@users.noreply.github.com>
2022-11-22 07:51:04 -0500

    updated schemas.ts for a deepscan syntax issue

--- src/app/modules/dex/schemas.ts
+++ src/app/modules/dex/schemas.ts
@@ -560,3 +560,38 @@ export const collectFeesSchema = {
         }
     }
 }
+
+export const removeLiquiditySchema = {
+    $id: '/dex/removeLiquidity',
+    "type": "object",
+    "required": [
+        "positionID",
+        "liquidityToRemove",
+        "amount0Min",
+        "amount1Min",
+        "maxTimestampValid"
+    ],
+    "properties": {
+        "positionID": {
+            "dataType": "bytes",
+            "fieldNumber": 1
+        },
+        "liquidityToRemove": {
+            "dataType": "uint64",
+            "fieldNumber": 2
+        },
+        "amount0Min": {
+            "dataType": "uint64",
+            "fieldNumber": 3
+        },
+        "amount1Min": {
+            "dataType": "uint64",
+            "fieldNumber": 4
+        },
+        "maxTimestampValid": {
+            "dataType": "uint64",
+            "fieldNumber": 5
+        },
+
+    }
+}
