26c26fdc18f71e81381ed034bc44655e4e27ad74
Irfan_Mustafa <irfanmustafa31201@gmail.com>
2023-01-09 00:12:29 +0500

    added getToken1Amount to endpoint

--- src/app/modules/dex/module.ts
+++ src/app/modules/dex/module.ts
@@ -41,7 +41,7 @@ import { CollectFeesCommand } from './commands/collectFees';
 import { RemoveLiquidityFailedEvent } from './events/removeLiquidityFailed';
 import { RemoveLiquidityEvent } from './events/removeLiquidity';
 import { RemoveLiquidityCommand } from './commands/removeLiquidity';
-import { getAllPoolIdsRequestSchema, getAllPoolIdsResponseSchema } from './schemas';
+import { getAllPoolIdsRequestSchema, getAllPoolIdsResponseSchema, getToken1AmountRequestSchema, getToken1AmountResponseSchema } from './schemas';
 
 export class DexModule extends BaseModule {
 	public id = MODULE_ID_DEX;
@@ -88,11 +88,18 @@ export class DexModule extends BaseModule {
 	public metadata(): ModuleMetadata {
 		return {
 			name: this.name,
-			endpoints: [{
+			endpoints: [
+			{
 				name: this.endpoint.getAllPoolIDs.name,
 				request: getAllPoolIdsRequestSchema,
 				response: getAllPoolIdsResponseSchema,
-			}],
+			},
+			{
+				name: this.endpoint.getToken1Amount.name,
+				request: getToken1AmountRequestSchema,
+				response: getToken1AmountResponseSchema,
+			},
+		],
 			commands: this.commands.map(command => ({
 				name: command.name,
 				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
