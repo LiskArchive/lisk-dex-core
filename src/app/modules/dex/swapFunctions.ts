bd0e406ef455896185d2c068ee5674019d43772f
Irfan_Mustafa <irfanmustafa31201@gmail.com>
2023-01-19 17:52:29 +0500

    added swapWithin function

---
+++ src/app/modules/dex/swapFunctions.ts
@@ -0,0 +1,66 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { computeNextPrice, getAmount0Delta, getAmount1Delta } from "./utils/math";
+
+export const swapWithin = (
+	sqrtCurrentPrice: bigint,
+	sqrtTargetPrice: bigint,
+	liquidity: bigint,
+	amountRemaining: bigint,
+	exactInput: boolean,
+): [bigint, bigint, bigint] => {
+	const zeroToOne: boolean = sqrtCurrentPrice >= sqrtTargetPrice;
+	let amountIn = BigInt(0);
+	let amountOut = BigInt(0);
+	let sqrtUpdatedPrice = BigInt(0);
+
+	if (exactInput) {
+		if (zeroToOne) {
+			amountIn = getAmount0Delta(sqrtCurrentPrice, sqrtTargetPrice, liquidity, true);
+		} else {
+			amountIn = getAmount1Delta(sqrtCurrentPrice, sqrtTargetPrice, liquidity, true);
+		}
+	} else if (zeroToOne) {
+		amountOut = getAmount1Delta(sqrtCurrentPrice, sqrtTargetPrice, liquidity, false);
+	} else {
+		amountOut = getAmount0Delta(sqrtCurrentPrice, sqrtTargetPrice, liquidity, false);
+	}
+	if (
+		(exactInput && amountRemaining >= amountIn) ||
+		(!exactInput && amountRemaining >= amountOut)
+	) {
+		sqrtUpdatedPrice = sqrtTargetPrice;
+	} else {
+		sqrtUpdatedPrice = computeNextPrice(
+			sqrtCurrentPrice,
+			liquidity,
+			amountRemaining,
+			zeroToOne,
+			exactInput,
+		);
+	}
+	if (zeroToOne) {
+		amountIn = getAmount0Delta(sqrtCurrentPrice, sqrtUpdatedPrice, liquidity, true);
+		amountOut = getAmount1Delta(sqrtCurrentPrice, sqrtUpdatedPrice, liquidity, false);
+	} else {
+		amountIn = getAmount1Delta(sqrtCurrentPrice, sqrtUpdatedPrice, liquidity, true);
+		amountOut = getAmount0Delta(sqrtCurrentPrice, sqrtUpdatedPrice, liquidity, false);
+	}
+	return [sqrtUpdatedPrice, amountIn, amountOut];
+};
