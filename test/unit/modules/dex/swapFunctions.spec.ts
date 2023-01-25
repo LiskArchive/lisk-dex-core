cc82749003b24d4326c6fde8506c1e925ae93291
Daniel-Lightcurve <daniel.salo@lightcurve.io>
2023-01-25 17:50:41 +0000

    refactor: resolve deepScan isseu

--- test/unit/modules/dex/swapFunctions.spec.ts
+++ test/unit/modules/dex/swapFunctions.spec.ts
@@ -233,7 +233,7 @@ describe('dex:swapFunctions', () => {
             poolAmountIn,
             exactIn
           );
-          bestRoute.push(optimalPool);
+          bestRoute.concat(optimalPool);
           poolTokenIn = regularRouteRt;
           poolAmountIn = poolAmountout;
         }
