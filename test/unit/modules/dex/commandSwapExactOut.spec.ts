04536655c0b53eb26c1f89b0f701eea40a94d01d
Irfan Mustafa <irfanmustafa31201@gmail.com>
2023-02-08 14:38:45 -0500

    resolved linting issues

--- test/unit/modules/dex/commandSwapExactOut.spec.ts
+++ test/unit/modules/dex/commandSwapExactOut.spec.ts
@@ -52,7 +52,6 @@ describe('swapEactOutn', () => {
 	const lockMock = jest.fn();
 	const unlockMock = jest.fn();
 
-
 	let poolsStore: PoolsStore;
 	let dexGlobalStore: DexGlobalStore;
 	let priceTicksStore: PriceTicksStore;
@@ -114,7 +113,6 @@ describe('swapEactOutn', () => {
 		tokenMethod.transfer = transferMock;
 		tokenMethod.lock = lockMock;
 		tokenMethod.unlock = unlockMock;
-
 
 		const currentTick = priceToTick(bytesToQ96(poolsStoreData.sqrtPrice));
 		const currentTickID = q96ToBytes(BigInt(currentTick));
