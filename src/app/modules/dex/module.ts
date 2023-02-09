f912dc85c4ed2cd556b97ee995f2b41d07949245
Daniel-Lightcurve <daniel.salo@lightcurve.io>
2023-02-09 14:44:07 +0000

    refactor: resolve odule issue

--- src/app/modules/dex/module.ts
+++ src/app/modules/dex/module.ts
@@ -132,13 +132,7 @@ export class DexModule extends BaseModule {
 
 	public metadata(): ModuleMetadata {
 		return {
-			stores: [
-				{ key: DexGlobalStore.name, data: dexGlobalStoreSchema },
-				{ key: PoolsStore.name, data: poolsStoreSchema },
-				{ key: PositionsStore.name, data: positionsStoreSchema },
-				{ key: PriceTicksStore.name, data: priceTicksStoreSchema },
-				{ key: SettingsStore.name, data: settingsStoreSchema },
-			],
+			name: this.name,
 			endpoints: [
 				{
 					name: this.endpoint.getAllPoolIDs.name,
