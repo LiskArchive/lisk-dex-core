830ffce98e3a1faaae3e6cd7dfb21682187c31ef
Irfan_Mustafa <irfanmustafa31201@gmail.com>
2023-02-10 10:24:46 -0500

    registered command in module.ts

--- src/app/modules/dex/module.ts
+++ src/app/modules/dex/module.ts
@@ -82,6 +82,7 @@ import { poolsStoreSchema } from './stores/poolsStore';
 import { positionsStoreSchema } from './stores/positionsStore';
 import { priceTicksStoreSchema } from './stores/priceTicksStore';
 import { settingsStoreSchema } from './stores/settingsStore';
+import { SwapExactWithPriceLimitCommand } from './commands/swapWithPriceLimit';
 
 export class DexModule extends BaseModule {
 	public id = MODULE_ID_DEX;
@@ -96,6 +97,7 @@ export class DexModule extends BaseModule {
 	private readonly _createPositionCommand = new CreatePositionCommand(this.stores, this.events);
 	private readonly _collectFeeCommand = new CollectFeesCommand(this.stores, this.events);
 	private readonly _removeLiquidityCommand = new RemoveLiquidityCommand(this.stores, this.events);
+	private readonly _swapExactWithPriceLimitCommand = new SwapExactWithPriceLimitCommand(this.stores, this.events);
 
 	// eslint-disable-next-line @typescript-eslint/member-ordering
 	public commands = [
@@ -104,6 +106,7 @@ export class DexModule extends BaseModule {
 		this._removeLiquidityCommand,
 		this._addLiquidityCommand,
 		this._createPositionCommand,
+		this._swapExactWithPriceLimitCommand
 	];
 
 	public constructor() {
@@ -124,7 +127,6 @@ export class DexModule extends BaseModule {
 		this.events.register(RemoveLiquidityEvent, new RemoveLiquidityEvent(this.name));
 		this.events.register(RemoveLiquidityFailedEvent, new RemoveLiquidityFailedEvent(this.name));
 		this.events.register(SwapFailedEvent, new SwapFailedEvent(this.name));
-
 		this.events.register(SwappedEvent, new SwappedEvent(this.name));
 	}
 
@@ -269,6 +271,10 @@ export class DexModule extends BaseModule {
 		});
 
 		this._removeLiquidityCommand.init({
+			tokenMethod: this._tokenMethod,
+		});
+
+		this._swapExactWithPriceLimitCommand.init({
 			tokenMethod: this._tokenMethod,
 		});
 	}
