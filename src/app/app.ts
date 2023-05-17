539f47836fa516f7a841a6483d09785f4569e960
Emilio <emilio.perez.sanchez@lightcurve.io>
2023-05-17 10:17:29 +0100

    Ishan suggestions to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,70 +1,26 @@
 import {
 	Application,
 	PartialApplicationConfig,
-	AuthModule,
-	ValidatorsModule,
-	TokenModule,
-	FeeModule,
-	PoSModule,
-	RandomModule,
-	SidechainInteroperabilityModule,
 } from 'lisk-sdk';
-import { DynamicRewardModule } from 'lisk-framework/dist-node/modules/dynamic_rewards';
 
 import { DexModule, DexIncentivesModule, DexGovernanceModule } from './modules';
 
 export const getApplication = (config: PartialApplicationConfig): Application => {
-	const app = new Application(config);
+	const { app, method } = Application.defaultApplication(config);
 	const dexModule = new DexModule();
-	const authModule = new AuthModule();
-	const validatorModule = new ValidatorsModule();
-	const tokenModule = new TokenModule();
-	const feeModule = new FeeModule();
-	const interoperabilityModule = new SidechainInteroperabilityModule();
-	const posModule = new PoSModule();
-	const randomModule = new RandomModule();
-	const dynamicRewardModule = new DynamicRewardModule();
 	const dexIncentivesModule = new DexIncentivesModule();
 	const dexGovernanceModule = new DexGovernanceModule();
 
-	// resolve dependencies
-	interoperabilityModule.addDependencies(validatorModule.method, tokenModule.method);
-	feeModule.addDependencies(tokenModule.method, interoperabilityModule.method);
-	dynamicRewardModule.addDependencies(
-		tokenModule.method,
-		randomModule.method,
-		validatorModule.method,
-		posModule.method,
-	);
-	posModule.addDependencies(
-		randomModule.method,
-		validatorModule.method,
-		tokenModule.method,
-		feeModule.method,
-	);
-	tokenModule.addDependencies(interoperabilityModule.method, feeModule.method);
-
-	// resolve interoperability dependencies
-	interoperabilityModule.registerInteroperableModule(tokenModule);
-	interoperabilityModule.registerInteroperableModule(feeModule);
-
-	dexModule.addDependencies(tokenModule.method, validatorModule.method, feeModule.method);
+	dexModule.addDependencies(method.token, method.validator, method.fee);
 	dexIncentivesModule.addDependencies(
-		tokenModule.method,
-		validatorModule.method,
-		randomModule.method,
-		feeModule.method,
-		posModule.method,
+		method.token,
+		method.validator,
+		method.random,
+		method.fee,
+		method.pos,
 	);
-	dexGovernanceModule.addDependencies(tokenModule.method, posModule.method);
-	app.registerModule(authModule);
-	app.registerModule(validatorModule);
-	app.registerModule(tokenModule);
-	app.registerModule(feeModule);
-	app.registerModule(interoperabilityModule);
-	app.registerModule(posModule);
-	app.registerModule(randomModule);
-	app.registerModule(dynamicRewardModule);
+	dexGovernanceModule.addDependencies(method.token, method.pos);
+
 	app.registerModule(dexModule);
 	app.registerModule(dexIncentivesModule);
 	app.registerModule(dexGovernanceModule);
